33c0bcf6f51e041e4a7a5632bd4dd0cb4c4ea77f
M ARIF SATRIA <arifsatria5554@gmail.com>
2023-11-22 16:17:33 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/habitapp/data/HabitRepository.kt
@@ -0,0 +1,69 @@
+package com.dicoding.habitapp.data
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.dicoding.habitapp.utils.HabitSortType
+import com.dicoding.habitapp.utils.SortUtils
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class HabitRepository(private val habitDao: HabitDao, private val executor: ExecutorService) {
+
+    companion object {
+
+        @Volatile
+        private var instance: HabitRepository? = null
+        private const val PAGE_SIZE = 10
+
+        fun getInstance(context: Context): HabitRepository {
+            return instance ?: synchronized(this) {
+                if (instance == null) {
+                    val database = HabitDatabase.getInstance(context)
+                    instance = HabitRepository(
+                        database.habitDao(),
+                        Executors.newSingleThreadExecutor()
+                    )
+                }
+                return instance as HabitRepository
+            }
+
+        }
+    }
+
+    //TODO 4 : Use SortUtils.getSortedQuery to create sortable query and build paged list done
+    fun getHabits(sortType: HabitSortType): LiveData<PagedList<Habit>> {
+        val query = SortUtils.getSortedQuery(sortType)
+
+        val dataSource = habitDao.getHabits(query)
+
+        val config = PagedList.Config.Builder()
+            .setPageSize(PAGE_SIZE)
+            .setEnablePlaceholders(true)
+            .build()
+        return LivePagedListBuilder(dataSource,config)
+            .build()
+    }
+
+    //TODO 5 : Complete other function inside repository done
+    fun getHabitById(habitId: Int): LiveData<Habit> {
+        return habitDao.getHabitById(habitId)
+    }
+
+    fun insertHabit(newHabit: Habit) {
+        executor.execute {
+            habitDao.insertHabit(newHabit)
+        }
+    }
+
+    fun deleteHabit(habit: Habit) {
+        executor.execute {
+            habitDao.deleteHabit(habit)
+        }
+    }
+
+    fun getRandomHabitByPriorityLevel(level: String): LiveData<Habit> {
+        return habitDao.getRandomHabitByPriorityLevel(level)
+    }
+}
---
+++ app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
@@ -0,0 +1,87 @@
+package com.dicoding.habitapp.notification
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.detail.DetailHabitActivity
+import com.dicoding.habitapp.utils.HABIT_ID
+import com.dicoding.habitapp.utils.HABIT_TITLE
+import com.dicoding.habitapp.utils.NOTIFICATION_CHANNEL_ID
+
+class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+
+    private val habitId = inputData.getInt(HABIT_ID, 0)
+    private val habitTitle = inputData.getString(HABIT_TITLE)
+
+    @SuppressLint("MissingPermission")
+    override fun doWork(): Result {
+        val prefManager = androidx.preference.PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val shouldNotify = prefManager.getBoolean(applicationContext.getString(R.string.pref_key_notify), false)
+
+        //TODO 12 : If notification preference on, show notification with pending intent done
+
+
+        if (shouldNotify) {
+
+            if (habitTitle != null) {
+                val notificationManager =
+                    applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+                val builder =
+                    NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_notifications)
+                        .setContentTitle(habitTitle)
+                        .setContentText(applicationContext.getString(R.string.notify_content))
+                        .setContentIntent(getPendingIntent())
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true)
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val channel = NotificationChannel(
+                        NOTIFICATION_CHANNEL_ID,
+                        "channelName",
+                        NotificationManager.IMPORTANCE_DEFAULT
+                    )
+                    builder.setChannelId(NOTIFICATION_CHANNEL_ID)
+                    notificationManager.createNotificationChannel(channel)
+                }
+
+                with(NotificationManagerCompat.from(applicationContext)) {
+                    notify(1, builder.build())
+                }
+            }
+        }
+
+
+        return Result.success()
+    }
+
+    private fun getPendingIntent(): PendingIntent? {
+        val intent = Intent(applicationContext, DetailHabitActivity::class.java).apply {
+            putExtra(HABIT_ID,habitId)
+        }
+
+        return TaskStackBuilder.create(applicationContext).run {
+            addNextIntentWithParentStack(intent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                getPendingIntent(
+                    0,PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+            } else {
+                getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+        }
+
+    }
+
+}
